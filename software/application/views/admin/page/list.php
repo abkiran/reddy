757e3e05ce384940f9cf48a0517b8008fe75c4a2
root <root@ip-172-31-18-63.us-west-2.compute.internal>
2015-12-27 05:46:16 +0000

    ci duplicated to software

---
+++ software/application/views/admin/page/list.php
@@ -0,0 +1,75 @@
+	<div class="contentpanel">
+	  <!--\\\\\\\ contentpanel start\\\\\\-->
+	<?php $this->load->view('admin/html/breadcrum', $this->data); ?>
+	  <div class="container clear_both padding_fix">
+		<!--\\\\\\\ container  start \\\\\\-->
+	  <!-- <div class="alert alert-success"> <strong>Well done!</strong> You successfully read <a class="alert-link" href="#">this important alert message</a>. </div> -->
+	  <div id="main-content">
+	<div class="page-content">
+	  
+	  
+	  <div class="row">
+		<div class="col-md-12">
+		  <div class="block-web">
+		   <div class="header">
+			  <div class="actions"> <a class="minimize" href="#"><i class="fa fa-chevron-down"></i></a> <a class="refresh" href="#"><i class="fa fa-repeat"></i></a> <a class="close-down" href="#"><i class="fa fa-times"></i></a> </div>
+			  <h3 class="content-header">Diet List</h3>
+			</div>
+		 <div class="porlets-content">
+			<div class="table-responsive">
+				<table  class="display table table-bordered table-striped" id="dynamic-table">
+				  <thead>
+					<tr>
+					  <th>Diet Code</th>
+					  <th>Diet Name</th>
+					  <th></th>
+					</tr>
+				  </thead>
+				  <tbody>
+				  <?php for ( $i = 0; $i < $rows[0]['NROWS']; $i++ ) { ?>
+					<tr class="gradeX" id="row-<?php echo $rows[$i]['id']; ?>">
+					  <td><a href="<?php echo site_url(); ?>/admin/page/modify/<?php echo $rows[$i]['id']; ?>"><?php echo $rows[$i]['diet_code']; ?></a></td>
+					  <td><?php echo $rows[$i]['diet_name']; ?></td>
+					  <td><a class="close-down remove" id="<?php echo $rows[$i]['id']; ?>" href="#"><i class="fa fa-times"></i></a> </td>
+					</tr>
+					<?php } ?>
+				  </tfoot>
+				</table>
+			  </div><!--/table-responsive-->
+			</div><!--/porlets-content-->
+			
+			
+		  </div><!--/block-web--> 
+		</div><!--/col-md-12--> 
+	  </div><!--/row-->
+	  
+		</div><!--/page-content end--> 
+	</div><!--/main-content end--> 
+	</div>
+	<script src="<?php echo base_url(); ?>assets/admin/js/jquery-2.1.0.js"></script>
+	<script src="<?php echo base_url(); ?>assets/admin/js/bootstrap.min.js"></script>
+	<script src="<?php echo base_url(); ?>assets/admin/js/jquery.slimscroll.min.js"></script>
+	<script src="<?php echo base_url(); ?>assets/admin/plugins/data-tables/jquery.dataTables.js"></script>
+	<script src="<?php echo base_url(); ?>assets/admin/plugins/data-tables/DT_bootstrap.js"></script>
+	<script src="<?php echo base_url(); ?>assets/admin/js/jPushMenu.js"></script> 
+	
+<script>
+$(document).ready(function(){   
+
+    $(".remove").click(function()
+    {
+	var id=$(this).attr('id');
+     $.ajax({
+         type: "POST",
+         url: "<?php echo site_url(); ?>/admin/removepage/", 
+         data: {id: id},
+         success: 
+              function(data){
+                alert(data);  //as a debugging message.
+				$("#row-"+id).fadeOut("slow");
+              }
+          });// you have missed this bracket
+     return false;
+ });
+ });
+</script>
---
+++ software/application/views/admin/page/record.php
@@ -0,0 +1,86 @@
+ <div class="contentpanel">
+      <!--\\\\\\\ contentpanel start\\\\\\-->
+      <?php $this->load->view('admin/html/breadcrum', $this->data); ?> 
+        <!--\\\\\\\ container  start \\\\\\-->
+	<?php if ( $page == 'add' ) { ?>
+	<div class="container clear_both padding_fix">
+      <div class="row">
+        <div class="col-md-10">
+          <div class="block-web">
+            <div class="header">
+              <div class="actions"> <a class="minimize" href="#"><i class="fa fa-chevron-down"></i></a><a class="close-down" href="#"><i class="fa fa-times"></i></a> </div>
+              <h3 class="content-header">Add a Diet item</h3>
+            </div>
+            <div class="porlets-content">
+              <form action="" method='post' class="form-horizontal row-border">
+			  <input type=hidden name='do' value="save">
+                <div class="form-group">
+                  <label class="col-sm-3 control-label">Diet Code</label>
+                  <div class="col-sm-9">
+                    <input type="text" class="form-control" name="diet_code" >
+                  </div>
+                </div><!--/form-group-->
+				<div class="form-group">
+                  <label class="col-sm-3 control-label">Diet Name</label>
+                  <div class="col-sm-9">
+                    <input type="text" class="form-control" name="diet_name" >
+                  </div>
+                </div><!--/form-group-->
+				<div class="form-group">
+                  <label class="col-sm-3 control-label">Add One More Item</label>
+                  <div class="col-sm-9">
+                    <input type="checkbox" class="form-control" name="add_more" > 
+                  </div>
+                </div><!--/form-group-->
+				<div class="bottom center">
+                  <button type="submit" class="btn btn-primary">Submit</button>
+                  <button type="button" class="btn btn-default">Cancel</button>
+                </div>
+			</form>
+		</div><!--/porlets-content-->
+	  </div><!--/block-web--> 
+	</div><!--/col-md-6-->
+	</div>
+</div>
+<?php } else {
+	if ( $rows[0]['NROWS'] > 0 ) {
+  ?>
+	<div class="container clear_both padding_fix">
+      <div class="row">
+        <div class="col-md-10">
+          <div class="block-web">
+            <div class="header">
+              <div class="actions"> <a class="minimize" href="#"><i class="fa fa-chevron-down"></i></a><a class="close-down" href="#"><i class="fa fa-times"></i></a> </div>
+              <h3 class="content-header">Update a Diet item</h3>
+            </div>
+            <div class="porlets-content">
+              <form action="" method='post' class="form-horizontal row-border">
+			  <input type=hidden name='do' value="update">
+			  <input type=hidden name='id' value="<?php echo $id; ?>">
+                <div class="form-group">
+                  <label class="col-sm-3 control-label">Diet Code</label>
+                  <div class="col-sm-9">
+                    <input type="text" name="diet_code" value="<?php echo $rows[0]['diet_code']; ?>" class="form-control">
+                  </div>
+                </div><!--/form-group-->
+				<div class="form-group">
+                  <label class="col-sm-3 control-label">Diet Name</label>
+                  <div class="col-sm-9">
+                    <input type="text" name="diet_name" class="form-control" value="<?php echo $rows[0]['diet_name']; ?>" >
+                  </div>
+                </div><!--/form-group-->
+				<div class="bottom center">
+                  <button type="submit" class="btn btn-primary">Submit</button>
+                  <button type="button" class="btn btn-default">Cancel</button>
+                </div>
+			</form>
+		</div><!--/porlets-content-->
+	  </div><!--/block-web--> 
+	</div><!--/col-md-6-->
+	</div>
+</div>
+<?php }else {
+	echo "invalid Item";
+}
+ } ?>
+<script type="text/javascript" src="<?php echo base_url(); ?>/assets/admin/plugins/ckeditor/ckeditor.js"></script>
