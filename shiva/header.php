ecab242fed6c79cde166c2df6998432dc96fb56c
abkiran <kiranreddyk@hotmail.com>
2017-12-11 16:27:36 +0530

    tawk

--- shiva/header.php
+++ shiva/header.php
@@ -39,15 +39,27 @@ if ( $_SERVER['HTTP_HOST'] != 'localhost' ){ ?>
   ga('send', 'pageview');
 
    //chat
-window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
+/*window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
 d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
 _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute("charset","utf-8");
 $.src="https://v2.zopim.com/?5KIluR7JGGnxJODo9yU9Sg6z2m4iV3ml";z.t=+new Date;$.
 type="text/javascript";e.parentNode.insertBefore($,e)})(document,"script");
-	
+*/	
 <?php } ?>
 </script>
-
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5a2e641ad0795768aaf8e8c1/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
 
 </head>
 <body class="slimScrollDiv">
